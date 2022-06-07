f0609dac145a21ee58b32785f6507cbbb5d59908
ThroughEndurance <103070722+ThroughEndurance@users.noreply.github.com>
2022-06-07 13:45:20 -0600

    Added begin game function and changed the deck.
    Changed the deck from hardcode to a generated deck using my buildDeck function.
    Added a begin game function which gives the dealer his facedown card and tallies it to dealerTotal.

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 let freshDeck = [];   //I'll want this to update with each new shuffle of the deck... I might not need this.
-let activeDeck = [];    //I'll want this to track the cards left in the deck/the cards that have been used so we don't get duplicates (use push/pop???)
+let activeDeck = [];    //I'll want this to track the cards left in the deck/the cards that have been used so we don't get duplicates? Maybe not needed it looks like (use push/pop???)
 let playerTotal = 0;    //I'll want this update on the fly as cards are dealt/when the player hits  (use reduce on player's array???)
 let dealerTotal = 0;    //I want this to update on the fly as the dealer gets cards (use reduce on dealers array????)
 let playerArray = [];   //I want this to track the players hand "behind the scenes"
 let dealerArray = [];   //I wan tthis to track the dealer's hand "behind the scenes"
 
-let deck = ['2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS'] //An array with my a deck of cards (in order) in it. I'm not sure if this is the way or not yet but I'm starting with this to get going.
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const masterDeck = buildDeck();
 
-const cardValue = 0; //I want to give all the cards in the deck a numueral value to track the math of the game.
+function buildDeck() {
+    const deck = [];
+    suits.forEach(function (suit) {
+        ranks.forEach(function (rank) {
+            deck.push({
+                card: `${suit}${rank}`,
+                value: Number(rank) || (rank === 'A' ? 11 : 10)
+            });
+        });
+    });
+    return deck;
+}
+
+//An array with my a deck of cards (in order) in it. I'm not sure if this is the way or not yet but I'm starting with this to get going.
+
+// const cardValue = 0; //I want to give all the cards in the deck a numueral value to track the math of the game. Also probably not needed....
 
 const hit = function(){
     //something that allows me to get the next card from the deck.
@@ -23,9 +40,9 @@ const stay = function(){
 const shuffle = function shuffleDeck(){
     for(let i = 0; i < 51; i++){
         let newCardIndex = Math.floor(Math.random() * 51);
-        let randomCard = deck[i];
-        deck[i] = deck[newCardIndex];
-        deck[newCardIndex] = randomCard;
+        let randomCard = masterDeck[i];
+        masterDeck[i] = masterDeck[newCardIndex];
+        masterDeck[newCardIndex] = randomCard;
     }
 }
 
@@ -44,4 +61,11 @@ const deal = function(){
 };
 
 
-console.log(deck)
+
+function beginGame(){
+    faceDown = masterDeck.pop();
+    dealerTotal += faceDown.value;
+    console.log(dealerTotal);
+    console.log(faceDown);
+}
+
